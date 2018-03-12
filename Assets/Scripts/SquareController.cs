81a2ef3cfc9c34d771c492e9e83c050242c4e9c7
Alex <alexx_88_88@mail.ru>
2018-03-12 23:03:50 +0300

    Test Drag&Throw component
    - реализовал тестовый компонент - контроллер перетаскивания выбранного объекта;

---
+++ Assets/Scripts/SquareController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SquareController : MonoBehaviour
+{
+
+	[SerializeField] private Text lable;
+	private Vector3 startPosition;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		startPosition = this.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnMouseDown()
+	{
+		Debug.Log("Test");
+		lable.text = "Test";
+	}
+
+
+	private Vector3 mousePosition;
+	void OnMouseDrag()
+	{
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		this.transform.position = new Vector3(mousePosition.x, mousePosition.y);
+
+//		if (spring.enabled = true) 
+//		{
+// 
+//			Vector2 cursorPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);//getting cursor position
+//                 
+//			spring.connectedAnchor = cursorPosition;//the anchor get's cursor's position
+//                  
+// 
+//		}
+	}
+ 
+     
+	void OnMouseUp()        
+	{
+     
+		//spring.enabled = false;//disabling the spring component
+		this.transform.position = startPosition;
+	}
+}

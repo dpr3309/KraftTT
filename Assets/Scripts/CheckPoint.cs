7bfaddc70af0c9c1245955ac8c06e8f804690360
Alex <alexx_88_88@mail.ru>
2018-03-13 08:40:52 +0300

    CheckPoint
    начал реализацию управляюего компонента  контрольной точки

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class CheckPoint : MonoBehaviour
+{
+	private SpriteRenderer spriteRenderer;
+
+	[SerializeField] private Color color;
+	public Color CurrentColor
+	{
+		get { return color; }
+	}
+
+	void Awake()
+	{
+		spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+		
+	}
+
+	private void Start()
+	{
+		spriteRenderer.color = color;
+	}
+}

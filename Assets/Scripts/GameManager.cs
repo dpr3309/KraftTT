8c817fbf2649d879f5ee660ff2b13b54661273f6
Alex <alexx_88_88@mail.ru>
2018-03-13 08:44:10 +0300

    ScoreManager & GameManager
    начал реализацию менеджера очков и основного игрового менеджера;

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	
+	
+	void Start () {
+		SquareController.ReachedCheckpoint += ReachedCheckpoint;
+		
+	}
+
+	private void ReachedCheckpoint(int point)
+	{
+		ScoreManager.Instance.AddPoint(point);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+	[SerializeField] private Text currentScoreLable, bestScoreLable;
+	private int currentScore, bestScore;
+
+	private static ScoreManager instance;
+	public static ScoreManager Instance
+	{
+		get { return instance; }
+	}
+
+	private void Awake()
+	{
+		if (Instance)
+		{
+			DestroyImmediate(this);
+			return;
+		}
+
+		instance = this;
+	}
+
+	void Start ()
+	{
+		LoadBestScore();
+	}
+
+	private void LoadBestScore()
+	{
+		//todo: загрузить лучший счет
+		UpdateBestScoreLable();
+	}
+
+	public void AddPoint(int point)
+	{
+		currentScore += point;
+		if (currentScore > bestScore)
+		{
+			bestScore = currentScore;
+			UpdateBestScoreLable();
+		}
+
+		UpdateCurrentScoreLable();
+	}
+
+	private void UpdateCurrentScoreLable()
+	{
+		currentScoreLable.text = currentScore.ToString();
+	}
+
+	private void UpdateBestScoreLable()
+	{
+		bestScoreLable.text = bestScore.ToString();
+	}
+}
